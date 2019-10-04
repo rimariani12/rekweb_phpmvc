419382b7b5977b2559b9b8ce5e9f952287dca7bb
Rima Riani <rima_173040117@mail.unpas.ac.id>
2019-10-04 19:56:17 +0700

    mengikuti video 7 dan 8

--- app/views/templates/footer.php
+++ app/views/templates/footer.php
@@ -2,4 +2,4 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="<?= BASEURL; ?>/js/bootstrap.js"></script>
 </body>
-</html>
+</html>  
